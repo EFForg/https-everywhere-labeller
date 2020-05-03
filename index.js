832e0b1cdee2ca10dab12952fbe294597d7d4b96
Alexis Hancock <lxshancock@gmail.com>
2020-05-02 17:15:46 -0700

    Update to reflect pending alexa data, testing

--- index.js
+++ index.js
@@ -1,177 +1,94 @@
+'use strict'
+
 const core = require('@actions/core');
 const github = require('@actions/github');
-const fs = require('fs');
-const readline = require('readline');
-const { Octokit } = require("@octokit/rest");
-const _ = require('lodash');
-const parseXML = require('xml2js').parseString;
 const axios = require('axios');
 const unzip = require('unzipper');
-const async = require('async');
 const context = github.context;
-
-
-const octokit = new Octokit({
-  auth: core.getInput('repo-token', { required: true }),
-  userAgent: 'Labeller v2'
-});
+const minimatch = require('minimatch')
+const rulesetGlob = new minimatch.Minimatch('/src/chrome/content/rules/*.xml')
 
 let ProgressBar = require('progress');
-let alexa_labels = ['top-1m', 'top-100k', 'top-10k', 'top-1k', 'top-100'];
+let alexaLabels = ['top-1m', 'top-100k', 'top-10k', 'top-1k', 'top-100'];
+let alexa = [];
+let regex = /^[0-9]+,(.+)/
+const alexa_csv = 'https://s3.amazonaws.com/alexa-static/top-1m.csv.zip';
 
-// Processing functions
-class Process {
-  labels(pr) {
-    // Check if Alexa labels already applied
-    let m = true;
+// Grab Alexa data
+axios({
+  method: 'get',
+  url: alexa_csv,
+  responseType: 'stream'
+})
+  .then(function (response) {
+    response.data.pipe(unzip.Parse())
+    .on('entry', function (entry) {
+      let bar = new ProgressBar('Processing Alexa Top 1M [:bar] :percent :etas', {
+        total: 100
+      });
 
-    pr.labels.forEach(element => {
-      if( alexa_labels.includes(element.name))
-        m = false;
-    });
+      let lineReader = require('readline').createInterface({
+        input: entry,
+      });
 
-    // Return filtered pull requests
-    return m;
-  }
+      let x = 0;
 
-  files(files, alexa) {
-    let rank;
+      lineReader.on('line', function (line) {
+        let domain = line.match(regex)[1];
+        alexa.push(domain);
+        if(x % 10000 == 0) bar.tick();
+        x++;
+      });
 
-    files.data.forEach(file => {
-      if(file.filename.match(/^src\/chrome\/content\/rules\//) !== null){
-
-        // Look at PR changes directly
-        let matches = file.patch.match(/((host)="([^"]|"")*")/g);
-
-        // strip to main domain
-        if( matches !== null) {
-          if( alexa.includes(matches[0].slice(6,-1))) {
-            let index = (matches[0].slice(6,-1))
-            rank = alexa.indexOf(index);
-            return rank;
-          }
+      lineReader.on('close', function(){
+        try {
+          run(alexa); // Intiates labelling
+        } catch (error) {
+          console.log(error);
         }
-      }
-    });
-    if(rank) {
-      return rank;
-    } else {
-      return null;
-    }
-  }
-
-  return_label(rank_num) {
-    let label;
-    if(rank_num < 100){
-      label = "top-100";
-    } else if(rank_num < 1000){
-      label = "top-1k";
-    } else if(rank_num < 10000){
-      label = "top-10k";
-    } else if(rank_num < 100000){
-      label = "top-100k";
-    } else {
-      label = "top-1m";
-    }
-    return label;
-  }
-
-  add_label(chosen_label, pr_number) {
-    octokit.issues.addLabels({
-      ...context.repo,
-      issue_number: pr_number,
-      labels: [chosen_label]
-    });
-  }
-}
-let process = new Process();
-
-/**
- * @description Fetch the Alexa top 1M sites and push it to an array `alexa` via streams
- * @returns object
- */
-function initiate() {
-  let alexa = [];
-  let regex = /^[0-9]+,(.+)/
-  const alexa_csv = 'https://s3.amazonaws.com/alexa-static/top-1m.csv.zip';
-
-  axios({
-    method: 'get',
-    url: alexa_csv,
-    responseType: 'stream'
-  })
-    .then(function (response) {
-      response.data.pipe(unzip.Parse())
-      .on('entry', function (entry) {
-        let bar = new ProgressBar('Processing Alexa Top 1M [:bar] :percent :etas', {
-          total: 100
-        });
-
-        let lineReader = require('readline').createInterface({
-          input: entry,
-        });
-
-        let x = 0;
-
-        lineReader.on('line', function (line) {
-          let domain = line.match(regex)[1];
-          alexa.push(domain);
-          if(x % 10000 == 0) bar.tick();
-          x++;
-        });
-
-        lineReader.on('close', function(){
-          try {
-            get_prs(alexa);
-          } catch (error) {
-            console.log(error);
-          }
-        });
-      })
+      });
     })
-    .catch(function (error) {
-      console.log(error);
-    });
-}
-
-function get_prs(alexa) {
-  let wildcard_www_regex = /^(www|\*)\.(.+)/
-
-  octokit.paginate(
-    "GET /repos/:owner/:repo/pulls",
-    ...context.repo,
-  )
-  .then(prs => {
-    process_prs(alexa, prs)
   })
-  .catch(reason => {
-    console.log(reason);
-  })
-}
-
-function process_prs(alexa, prs) {
-  let filtered_prs = prs.filter(process.labels);
-
-  prs.forEach(pr => {
-
-    let domain_label_pairs = [];
-
-    octokit.pulls.listFiles({
-      ...context.repo,
-      pull_number: pr.number,
-    }).then(files => {
-      let rank_number = process.files(files, alexa);
-      if(rank_number !== null) {
-        let determined_label = process.return_label(rank_number);
-        // pr is interchangeable with issue in API ¯\_(ツ)_/¯
-        process.add_label(determined_label, pr.number);
-      }
-    })
+  .catch(function (error) {
+    console.log(error);
   });
-}
 
-try {
-  initiate();
-} catch (error) {
-  core.setFailed(error.message);
+// Label PR if Needed
+async function run(alexa) {
+  const token = core.getInput('repo-token', { required: true });
+  const octokit = new github.GitHub(token);
+  const pR = context.payload.pull_request;
+
+  try {
+    if (context.payload.action !== 'opened' || !pR) {
+      return
+    }
+
+    const prNumber = pR.number
+
+    pR.labels.forEach(element => {
+      if( alexa_labels.includes(element.name))
+        return;
+    });
+
+    const response = await client.pulls.listFiles({
+      ...context.repo,
+      pull_number: prNumber
+    })
+    const fileList = response.data
+
+    if (!fileList.every(file => rulesetGlob.match(file.name))) {
+      // Don't touch PRs that modify anything except rulesets
+      return;
+    } else {
+      console.log(fileList);
+      console.log(alexa);
+      // Check file's domain and if it's in alexa domains
+      // strip to main domain
+      // If in Alexa domains, check rank,
+    }
+  } catch (err) {
+    core.error(err.stack)
+    core.setFailed(err.message)
+  }
 }
